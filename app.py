a39f2e73e3b69fe0669c6f74812713684a2cbbfe
Quentin Williams <quentinbwilliams@gmail.com>
2021-08-07 10:56:35 -0400

    added routes

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
-"""Blogly application."""
+"""Blog application."""
 
-from flask import Flask
+from flask import Flask, request, render_template,  redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2:////tmp/test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY']='fun'
+
+debug = DebugToolbarExtension(app)
 
 connect_db(app)
 db.create_all()
+
+@app.route('/')
+def home():
+    """ """
+    
+    return render_template('index.html')
+
+@app.route('/users')
+def show_all_users():
+    """ """
+    
+    return render_template('users.html')
+
+@app.route('/users', methods = ["POST"])
+def make_new_user():
+    """ """
+    
+    return render_template('users.html')
--- models.py
+++ models.py
@@ -1,1 +1,28 @@
-"""Models for Blogly."""
+"""Models for Blog."""
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    
+    db.init_app(app)
+
+
+class User(db.Model):
+    """ """
+    
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    
+    first_name = db.Column(db.String(80), unique=False, nullable=False)
+
+    last_name = db.Column(db.String(80), unique=False, nullable=False)
+    
+    image_url = db.Column(db.String(), unique=False, nullable=True)
